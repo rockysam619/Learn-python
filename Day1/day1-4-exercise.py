79fe1f3b7395f83a38683fc80d9c0d54db7df2be
Rohit Sharma <rockysam619@outlook.com>
2021-06-13 21:33:11 +0530

    Day1 of 100 days of python

---
+++ Day1/day1-4-exercise.py
@@ -0,0 +1,13 @@
+#Input the 2 valus a and b
+a = input("a: ")
+b = input("b: ")
+
+#Swap the values
+c = a
+a = b
+b = c
+
+#Print swapped values
+print("Swapping the values")
+print("a: ", a)
+print("b: ", b)
---
+++ Day1/day1-band-name-generator.py
@@ -0,0 +1,7 @@
+print("**Welcome to the band name generator!**\n")
+
+city_name = input("What is the name of the city you group up in?\n")
+
+pet_name = input("What was the name of your pet?\n")
+
+print("Your awesome band name is " + "*" + city_name + " " + pet_name + "*")
